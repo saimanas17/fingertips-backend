ec0402fb33bf9b482c5eaaa45e395cc4f839e88b
Saimanas Gourabathini <gourabathini.s@northeastern.edu>
2024-04-16 14:44:58 -0400

    CORS URL Change

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://fingertips-201005000.us-east-1.elb.amazonaws.com/"
+  );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
